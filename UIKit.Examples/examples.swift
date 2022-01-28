85a97beb8c70130fc185ae0a996c418f3b22f3db
Liu <bruce.liu@experian.com>
2022-01-28 00:56:14 -0800

    Update

--- UIKit.Examples/examples.swift
+++ UIKit.Examples/examples.swift
@@ -4,15 +4,17 @@ let examples: [ExampleSection] = [
     ExampleSection(title: "Controls", examples: [
         SwiftExample(title: "Switch", storyboard: "controls", identifier: "SwitchViewController"),
         SwiftExample(title: "Image", storyboard: "controls", identifier: "ImageViewController"),
-        SwiftExample(title: "Picker", storyboard: "controls", identifier: "PickerViewController"),
-        SwiftExample(title: "Date Picker", storyboard: "controls", identifier: "DatePickerViewController"),
-        SwiftExample(title: "Color Picker", storyboard: "controls", identifier: "ColorPickerViewController"),
-        SwiftExample(title: "Font Picker", storyboard: "controls", identifier: "FontPickerViewController")
     ]),
     ExampleSection(title: "Views", examples: [
         SwiftExample(title: "Collection", storyboard: "views", identifier: "CollectionViewController"),
         SwiftExample(title: "Spinner", storyboard: "views", identifier: "SpinnerViewController"),
         SwiftExample(title: "Tab Bar", storyboard: "views", identifier: "TabBarController")
+    ]),
+    ExampleSection(title: "Pickers", examples: [
+        SwiftExample(title: "Picker", storyboard: "pickers", identifier: "PickerViewController"),
+        SwiftExample(title: "Date Picker", storyboard: "pickers", identifier: "DatePickerViewController"),
+        SwiftExample(title: "Color Picker", storyboard: "pickers", identifier: "ColorPickerViewController"),
+        SwiftExample(title: "Font Picker", storyboard: "pickers", identifier: "FontPickerViewController")
     ])
 ]
 
